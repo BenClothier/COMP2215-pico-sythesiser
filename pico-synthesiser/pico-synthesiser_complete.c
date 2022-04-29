a2a5f15df06fd903412c415bb53cd98ca04037c6
Ben Clothier <ben@exabytegames.com>
2022-04-29 14:34:40 +0100

    Read ADC input from potentiometer

--- pico-synthesiser/pico-synthesiser_complete.c
+++ pico-synthesiser/pico-synthesiser_complete.c
@@ -5,7 +5,21 @@
  */
 
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    return 0;
+    stdio_init_all();
+    gpio_init(25);
+    adc_init();
+    adc_gpio_init(26);
+    adc_select_input(0);
+
+    while(true) {
+        uint16_t result = adc_read();
+        const float conversion_factor = 3.3f / (1 << 12);
+        printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor);
+        sleep_ms(1000);
+    }
 }
