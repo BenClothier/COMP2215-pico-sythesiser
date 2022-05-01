1901b2b8aea93bdc04ffb5bda3062cb15a925e90
Ben Clothier <ben@exabytegames.com>
2022-05-01 11:28:51 +0100

    Finish challenge file

--- pico-synthesiser/pico-synthesiser.c
+++ pico-synthesiser/pico-synthesiser.c
@@ -12,15 +12,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define VOLTAGE_IN_PIN 26
-#define AUDIO_OUT_PIN 18
+#define VOLTAGE_IN_PIN 26       // Desired ADC input pin (make sure to check which pins are compatible with the ADC if you want to change this)
+#define AUDIO_OUT_PIN 18        // Buzzer/AUX pin
 
-float input_voltage = 3.3f;
-float highest_freq = 1000.0f;
-float lowest_freq = 0.0f;
+float input_voltage = 3.3f;     // Voltage input of the potential divider circuit
+float highest_freq = 1000.0f;   // Highest frequency wave that you want to generate
+float lowest_freq = 0.0f;       // Lowest/Base frequency of the synthesiser
 
-uint slice_num;
-const uint16_t duty = 50;
+uint slice_num;                 // Slice number of the PWM
+const uint16_t duty = 50;       // Duty cycle for the PWM
 
 uint32_t voltageToFreq(float voltage) {
     return (uint32_t)(highest_freq - (voltage / input_voltage) * (highest_freq - lowest_freq));
@@ -31,11 +31,11 @@ float getVoltageFromADC() {
 }
 
 int pwm_set_freq(uint32_t freq) {
-    // Write your solution here...
+    // TODO: Configure the PWM to generate a wave of the given frequency...
 }
 
 void on_pwm_wrap() {
-    // Every pwm-wrap, measure the voltage input and recalculate the desired frequency
+    // Every pwm-wrap, measures the voltage input and recalculates the desired frequency
     float voltage = getVoltageFromADC();
     uint32_t freq = voltageToFreq(voltage);
     pwm_set_freq(freq);
@@ -44,12 +44,9 @@ void on_pwm_wrap() {
 int main() {
     stdio_init_all();
 
-    // Initialise ADC input
-    adc_init();
-    adc_gpio_init(VOLTAGE_IN_PIN);
-    adc_select_input(VOLTAGE_IN_PIN - 26);
+    // TODO: Initialise ADC here...
 
-    // Initialise PWM and on-wrap routine
+    // Initialises PWM and on-wrap routine
     gpio_set_function(AUDIO_OUT_PIN, GPIO_FUNC_PWM);
     slice_num = pwm_gpio_to_slice_num(AUDIO_OUT_PIN);
     pwm_clear_irq(slice_num);
